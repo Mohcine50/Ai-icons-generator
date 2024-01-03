e1136d0db61e90a78fbfadf87dddf76e8f6ff2ae
shegami <mohcinela1998@gmail.com>
2024-01-03 19:47:33 +0100

    change loading an api key

--- app/(landing-page)/collection/_components/collections.tsx
+++ app/(landing-page)/collection/_components/collections.tsx
@@ -29,35 +29,41 @@ const Collections = async () => {
 
   return (
     <>
-      {collections.map((prompt: collectionType, index: number) => {
-        return (
-          <>
-            <Accordion type="single" collapsible className="max-w-2xl w-full">
-              <AccordionItem value="item-1">
-                <AccordionTrigger>
-                  {prompt.promptProperties.character}
-                </AccordionTrigger>
-                <AccordionContent>
-                  <div className="flex gap-3 flex-wrap">
-                    {prompt.images.map((image: image, idx: number) => {
-                      return (
-                        <Image
-                          key={idx}
-                          src={image.imageUrl as string}
-                          alt={idx.toString()}
-                          width="120"
-                          height={120}
-                          className="rounded-lg  border-[3px] border-white shadow-sm"
-                        />
-                      );
-                    })}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </>
-        );
-      })}
+      {collections.length === 0 ? (
+        <h1 className="text-2xl text-center font-bold mt-5 p-2">
+          No prompt to show on your Collection
+        </h1>
+      ) : (
+        collections.map((prompt: collectionType, index: number) => {
+          return (
+            <>
+              <Accordion type="single" collapsible className="max-w-2xl w-full">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    {prompt.promptProperties.character}
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex gap-3 flex-wrap">
+                      {prompt.images.map((image: image, idx: number) => {
+                        return (
+                          <Image
+                            key={idx}
+                            src={image.imageUrl as string}
+                            alt={idx.toString()}
+                            width="120"
+                            height={120}
+                            className="rounded-lg  border-[3px] border-white shadow-sm"
+                          />
+                        );
+                      })}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </>
+          );
+        })
+      )}
     </>
   );
 };
--- app/(landing-page)/collection/loading.tsx
+++ app/(landing-page)/collection/loading.tsx
@@ -1,4 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <h1>Loading your prompts</h1>;
+  return (
+    <h1 className="text-2xl text-center font-bold mt-5 p-2">
+      Loading your prompts
+    </h1>
+  );
 }
