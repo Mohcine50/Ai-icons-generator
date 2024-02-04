8b7abafef2ed3b9d232c3f6711d7de6845d33853
shegami <mohcinela1998@gmail.com>
2024-02-04 16:04:01 +0100

    fix skeleton issue

--- app/(landing-page)/generate/_components/imageSkeleton.tsx
+++ app/(landing-page)/generate/_components/imageSkeleton.tsx
@@ -1,27 +1,15 @@
-"use client";
-
 import React, { useState } from "react";
 import Image from "next/image";
 
-import { Skeleton } from "@/components/ui/skeleton";
-
 const ImageSkeleton = ({ src, idx }: { src: string; idx: number }) => {
-  const [loadingCompleted, setLoadingCompleted] = useState(false);
   return (
-    <>
-      {loadingCompleted ? (
-        <Skeleton className="w-[120px] h-[120px] rounded-lg  border-[3px] border-white shadow-sm" />
-      ) : (
-        <Image
-          src={src}
-          alt={idx.toString()}
-          width={120}
-          height={120}
-          onLoadingComplete={() => setLoadingCompleted(true)}
-          className="rounded-lg  border-[3px] border-white shadow-sm"
-        />
-      )}
-    </>
+    <Image
+      src={src}
+      alt={idx.toString()}
+      width={120}
+      height={120}
+      className="rounded-lg  border-[3px] border-white shadow-sm"
+    />
   );
 };
 
